2f7bcd15d8c2cb62b171d434d8af32788606be70
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-08 21:47:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("please server...") {type= 2 "STREAMING"})
+  bot.user.setActivity("please server...") {type: "STREAMING"})
   bot.user.setActivity(`New update QQ!`);
 });
 
