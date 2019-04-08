586c6494ff80c24c8dceb9ce0e7c6f06f2f65b94
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-09 03:27:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bot = new Discord.Client({disableEveryone: true});
   bot.user.setActivity(`New! update QQ!`);
   bot.user.setUsername("QQ SPEED V1");
   bot.user.setStatus('online');
-  bot.user.setActivity(`on ${client.users.size} users  -help`, {
+  bot.user.setActivity(`on ${bot.users.size} users  -help`, {
     type: 'WATCHING'
   });
 
