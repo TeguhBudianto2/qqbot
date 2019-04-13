d36a559e1bc49e8581045f60425603fd8eaf40ed
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-13 13:14:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Activate ${bot.users.size} Users! `, {type: 'STREAMING'});
+  bot.user.setStatus('available')
+});
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '//';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}ping`){
+    message.channel.send("Pong!");
+  }
+  
+});
+
+bot.login(process.env.token);
