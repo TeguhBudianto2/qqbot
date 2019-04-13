afd400768ef11d4b0faed3b4172208b0bbbc8a37
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-13 13:33:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  sleep(90000)
         bot.user.setActivity(`${bot.users.size} Usuários`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        sleep(60
         bot.user.setActivity(`QUALQUER COISA`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        sleep(60000)
         bot.user.setActivity(`OLÁ CLIENT DEV NIKO`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        sleep(60000)
         bot.user.setActivity(`DALEE`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        sleep(80000)
         bot.user.setActivity(`NHAW NHAW`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        sleep(80000)
         bot.user.setActivity(`Digunakan ${bot.guilds.size} server!`, {type: 'STREAMING', url: 'https://twitch.tv/'})
 });
 
