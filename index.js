134dea66cadb08f231af0afd8eef0c94a4b16c9a
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-13 13:44:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
+        setTimeout(() => {
         bot.user.setActivity(`${bot.users.size} users`, {type: 'STREAMING', url: 'https://twitch.tv/'})
+        setTimeout(() => {
         bot.user.setActivity(`https://discord.gg/DPQgeFT`, {type: 'LISTENING', url: 'https://twitch.tv/'})
+        setTimeout(() => {
         bot.user.setActivity(`help | //help`, {type: 'WATCHING', url: 'https://twitch.tv/'})
+        setTimeout(() => {
         bot.user.setActivity(`By Skylar红色`, {type: 'STREAMING', url: 'https://twitch.tv/'})
+        setTimeout(() => {
         bot.user.setActivity(`Digunakan ${bot.guilds.array().length} server`, {type: "WATCHING"});
   }, 20000)
 });
