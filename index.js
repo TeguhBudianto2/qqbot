3df5a98042741cd86a8d4fad350960f9f7ee020e
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-13 13:37:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-        bot.user.setActivity(`${bot.users.size} Usuários`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        bot.user.setActivity(`QUALQUER COISA`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        bot.user.setActivity(`OLÁ CLIENT DEV NIKO`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        bot.user.setActivity(`DALEE`, {type: 'STREAMING', url: 'https://twitch.tv/'})
-        bot.user.setActivity(`NHAW NHAW`, {type: 'STREAMING', url: 'https://twitch.tv/'})
+        bot.user.setActivity(`${bot.users.size} users`, {type: 'STREAMING', url: 'https://twitch.tv/'})
+        bot.user.setActivity(`https://discord.gg/DPQgeFT`, {type: 'LISTENING', url: 'https://twitch.tv/'})
+        bot.user.setActivity(`help | //help`, {type: 'WATCHING', url: 'https://twitch.tv/'})
+        bot.user.setActivity(`By Skylar红色`, {type: 'STREAMING', url: 'https://twitch.tv/'})
         bot.user.setActivity(`Digunakan ${bot.guilds.size} server!`, {type: 'STREAMING', url: 'https://twitch.tv/'})
 });
 
