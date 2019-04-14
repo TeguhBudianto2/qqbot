e22876f0c4d9c4e029a0c6d4b9a63ba2b792eb6b
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-15 01:13:24 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,17 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+client.user.setPresence({
+        game: { 
+            name: 'PREFIX / BOT INFO',
+            type: 'WATCHING'
+        },
+        status: 'idle'
+    })
 
 client.on('message', msg => {
   if (msg.content === '/help') {
-    msg.reply('```Prefix is for /help server use now```,```wa```,```fb```,```taptap```');
+    msg.reply('```Prefix is for /help server use now``` ```wa``` ```fb``` ```taptap```');
   }
 });
 
