9f5f82f088b24105ab62bede6dc9fdccf581cd2f
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-15 01:17:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-client.user.setPresence({
+bot.user.setPresence({
         game: { 
             name: 'PREFIX / BOT INFO',
             type: 'WATCHING'
