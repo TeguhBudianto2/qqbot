08968fb427dcba8e8e5f79c66cde4dc7ddf6387b
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-15 11:41:55 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-client.user.setActivity('YouTube', { type: 'WATCHING' })
-  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch(console.error);
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
