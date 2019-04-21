d265bc5d9a6c2b8ff7e492211b699eddfea34e8d
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-21 11:17:42 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const activities_list = [
-    "with the &help command.", 
-    "with the developers console",
-    "with some code", 
-    "with JavaScript"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
-
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
+
 });
+
 
 client.on('message', msg => {
   if (msg.content === ',help') {
